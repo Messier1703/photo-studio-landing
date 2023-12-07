48ef210c228ef180502d7e6afa93fe156c9e1cef
Messier1703 <112243409+Messier1703@users.noreply.github.com>
2023-12-07 21:05:00 +0600

    feat: add tabs api integration

--- src/components/ui/PortfolioTabs/PortfolioTabs.tsx
+++ src/components/ui/PortfolioTabs/PortfolioTabs.tsx
@@ -1,62 +1,61 @@
 'use client'
 import { Tabs, TabList, Tab, TabPanel } from 'react-aria-components'
 import styles from './PortfolioTabs.module.scss'
-// import API_BASE_URL from '@/constants/API_BASE_URL'
-// import requestInterface from '@/constants/requestInterface'
-// import ky from 'ky'
-// import { useState, useEffect } from 'react'
+import API_BASE_URL from '@/constants/API_BASE_URL'
+import ky from 'ky'
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+
+interface Image {
+  id: number
+  image: string
+}
+
+interface PortfolioItem {
+  id: number
+  title: string
+  images: Image[]
+}
 
 const PortfolioTabs = () => {
-  // const [portfolio, setPortfolio] = useState<requestInterface[]>([])
+  const [portfolio, setPortfolio] = useState<PortfolioItem[]>([])
 
-  // useEffect(() => {
-  //   const getPortfolio = async () => {
-  //     try {
-  //       const response = await ky.get(`${API_BASE_URL}/our_team`).json<requestInterface[]>()
-  //       console.log(response)
-  //       setPortfolio(response)
-  //     } catch (error) {
-  //       console.error(error)
-  //     }
-  //   }
+  useEffect(() => {
+    const getPortfolio = async () => {
+      try {
+        const response = await ky.get(`${API_BASE_URL}/our_products`).json<PortfolioItem[]>()
+        console.log(response)
+        setPortfolio(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
-  //   getPortfolio()
-  // }, [])
+    getPortfolio()
+  }, [])
 
   return (
     <Tabs className={styles.tabs}>
       <TabList className={styles.tab_list}>
-        <Tab id='clothing' className={styles.tab}>
-          одежда
-        </Tab>
-        <Tab id='footwear' className={styles.tab}>
-          обувь
-        </Tab>
-        <Tab id='accessories' className={styles.tab}>
-          аксессуары
-        </Tab>
-        <Tab id='items' className={styles.tab}>
-          предметная съемка
-        </Tab>
-        <Tab id='info' className={styles.tab}>
-          инфографика
-        </Tab>
+        {portfolio.map((item) => (
+          <Tab key={item.id} id={item.title.toLowerCase()} className={styles.tab}>
+            {item.title}
+          </Tab>
+        ))}
       </TabList>
-      <TabPanel id='clothing' className={styles.tab_panel}>
-        <h3>одежда</h3>
-      </TabPanel>
-      <TabPanel id='footwear' className={styles.tab_panel}>
-        <h3>обувь</h3>
-      </TabPanel>
-      <TabPanel id='accessories' className={styles.tab_panel}>
-        <h3>аксессуары</h3>
-      </TabPanel>
-      <TabPanel id='items' className={styles.tab_panel}>
-        <h3>предметная съемка</h3>
-      </TabPanel>
-      <TabPanel id='info' className={styles.tab_panel}>
-        <h3>инфографика</h3>
-      </TabPanel>
+
+      {portfolio.map((item) => (
+        <TabPanel key={item.id} id={item.title.toLowerCase()} className={styles.tab_panel}>
+          <h3>{item.title}</h3>
+          <div className={styles.tab_image_wrapper}>
+            {item.images.map((image) => (
+              <figure key={image.id}>
+                <Image src={image.image} alt={`Image ${image.id}`} width={300} height={420} />
+              </figure>
+            ))}
+          </div>
+        </TabPanel>
+      ))}
     </Tabs>
   )
 }
