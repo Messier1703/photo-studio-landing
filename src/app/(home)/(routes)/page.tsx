95fc856407bafce4bb6f3c72be29a48498f1bd05
Messier1703 <112243409+Messier1703@users.noreply.github.com>
2023-12-10 20:12:33 +0600

    feat: finish InfographicsSection adaptive

--- src/app/(home)/(routes)/page.tsx
+++ src/app/(home)/(routes)/page.tsx
@@ -19,7 +19,7 @@ const HomePage = () => {
       {/* <AboutSection /> */}
       {/* <ServicesSection /> */}
       <ObjectSection />
-      {/* <InfographicsSection /> */}
+      <InfographicsSection />
       {/* <TeamSection /> */}
       {/* <ContactsSection /> */}
     </>
--- src/components/sections/InfographicsSection/InfographicsSection.tsx
+++ src/components/sections/InfographicsSection/InfographicsSection.tsx
@@ -7,24 +7,25 @@ import Link from 'next/link'
 import BrightButton from '@/components/ui/BrightButton/BrightButton'
 import Image from 'next/image'
 import placeholderText from '@/constants/placeholderText'
+import { StaticImageData } from 'next/image'
 
 const InfographicsSection = () => {
   interface GetInfographicsProps {
     title: string
     description: string
-    image_1: string
-    image_2: string
-    image_3: string
-    image_4: string
+    image_1: StaticImageData
+    image_2: StaticImageData
+    image_3: StaticImageData
+    image_4: StaticImageData
   }
 
   const [infographics, setInfographics] = useState<GetInfographicsProps>({
     title: `${placeholderText}`,
     description: `${placeholderText}`,
-    image_1: '',
-    image_2: '',
-    image_3: '',
-    image_4: '',
+    image_1: {} as StaticImageData,
+    image_2: {} as StaticImageData,
+    image_3: {} as StaticImageData,
+    image_4: {} as StaticImageData,
   })
 
   useEffect(() => {
@@ -45,6 +46,13 @@ const InfographicsSection = () => {
       <div className='container'>
         {infographics && (
           <div className={styles.infographics_wrapper}>
+            <div className={styles.infographics_content} id={styles.mobile_only}>
+              <h3>{infographics.title}</h3>
+              <p>{infographics.description}</p>
+              <Link href='/#contacts' id={styles.desktop_only}>
+                <BrightButton>Обсудить работу</BrightButton>
+              </Link>
+            </div>
             <div className={styles.infographics_images}>
               <figure className={styles.infographics_image_wrapper}>
                 <Image className={styles.infographics_image} src={infographics.image_1} alt='Фото' width={300} height={360} />
@@ -59,13 +67,16 @@ const InfographicsSection = () => {
                 <Image className={styles.infographics_image} src={infographics.image_4} alt='Фото' width={300} height={360} />
               </figure>
             </div>
-            <div>
+            <div className={styles.infographics_content} id={styles.desktop_only}>
               <h3>{infographics.title}</h3>
               <p>{infographics.description}</p>
               <Link href='/#contacts'>
                 <BrightButton>Обсудить работу</BrightButton>
               </Link>
             </div>
+            <Link href='/#contacts' id={styles.mobile_only}>
+              <BrightButton>Обсудить работу</BrightButton>
+            </Link>
           </div>
         )}
       </div>
