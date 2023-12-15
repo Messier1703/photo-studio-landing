94a86d8f506c426cb86e7ffc08c2c1462f18ea5b
Messier1703 <112243409+Messier1703@users.noreply.github.com>
2023-12-15 12:23:44 +0600

    fix: fix nav links

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,45 +1,45 @@
-import styles from './Footer.module.scss'
-import Link from 'next/link'
-import managerNumber from '@/constants/managerNumber'
-import vkLogo from 'public/svg/vk-logo.svg'
-import telegramLogo from 'public/svg/telegram-logo.svg'
-import whatsappLogo from 'public/svg/whatsapp-logo.svg'
-import Logo from '@/components/ui/Logo/Logo'
-import Image from 'next/image'
+import styles from "./Footer.module.scss"
+import Link from "next/link"
+import managerNumber from "@/constants/managerNumber"
+import vkLogo from "public/svg/vk-logo.svg"
+import telegramLogo from "public/svg/telegram-logo.svg"
+import whatsappLogo from "public/svg/whatsapp-logo.svg"
+import Logo from "@/components/ui/Logo/Logo"
+import Image from "next/image"
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className='container'>
+      <div className="container">
         <div className={styles.footer_wrapper}>
           <div>
-            <Link href='/'>
+            <Link href="/">
               <Logo />
             </Link>
           </div>
           <div id={styles.desktop_only}>
-            <Link href='/'>О нас</Link>
-            <Link href='/'>Наши работы</Link>
-            <Link href='/'>Услуги</Link>
-            <Link href='/'>Отзывы</Link>
-            <Link href='/'>Контакты</Link>
+            <Link href="/#about">О нас</Link>
+            <Link href="/#portfolio">Наши работы</Link>
+            <Link href="/#services">Услуги</Link>
+            <Link href="/#reviews">Отзывы</Link>
+            <Link href="#contacts">Контакты</Link>
           </div>
           <div>
-            <Link href='/'>Политика конфиденциальности</Link>
+            <Link href="/confidentiality">Политика конфиденциальности</Link>
           </div>
           <div>
-            <Link href='/' id={styles.footer_number}>
+            <Link href="/" id={styles.footer_number}>
               {managerNumber}
             </Link>
             <div className={styles.footer_icons}>
-              <Link href='/'>
-                <Image src={whatsappLogo} alt='WhatsApp' />
+              <Link href="/">
+                <Image src={whatsappLogo} alt="WhatsApp" />
               </Link>
-              <Link href='/'>
-                <Image src={vkLogo} alt='Vkontakte' />
+              <Link href="/">
+                <Image src={vkLogo} alt="Vkontakte" />
               </Link>
-              <Link href='/'>
-                <Image src={telegramLogo} alt='Telegram' />
+              <Link href="/">
+                <Image src={telegramLogo} alt="Telegram" />
               </Link>
             </div>
           </div>
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,16 +1,16 @@
-'use client'
-import Link from 'next/link'
-import styles from './Header.module.scss'
-import HeaderButton from '../../components/ui/BurgerButton/HeaderButton/HeaderButton'
-import managerNumber from '@/constants/managerNumber'
-import vkLogo from 'public/svg/vk-logo.svg'
-import whatsappLogo from 'public/svg/whatsapp-logo.svg'
-import telegramLogo from 'public/svg/telegram-logo.svg'
-import { useState, useEffect } from 'react'
-import Logo from '@/components/ui/Logo/Logo'
-import Image from 'next/image'
-import BurgerButton from '@/components/ui/BurgerButton/BurgerButton'
-import BurgerMenu from '@/components/ui/BurgerMenu/BurgerMenu'
+"use client"
+import Link from "next/link"
+import styles from "./Header.module.scss"
+import HeaderButton from "../../components/ui/BurgerButton/HeaderButton/HeaderButton"
+import managerNumber from "@/constants/managerNumber"
+import vkLogo from "public/svg/vk-logo.svg"
+import whatsappLogo from "public/svg/whatsapp-logo.svg"
+import telegramLogo from "public/svg/telegram-logo.svg"
+import { useState, useEffect } from "react"
+import Logo from "@/components/ui/Logo/Logo"
+import Image from "next/image"
+import BurgerButton from "@/components/ui/BurgerButton/BurgerButton"
+import BurgerMenu from "@/components/ui/BurgerMenu/BurgerMenu"
 
 const Header = () => {
   const [scroll, setScroll] = useState(false)
@@ -24,7 +24,7 @@ const Header = () => {
   const toggleScroll = () => {
     const scrollPosition = window.scrollY
 
-    const mainSection = document.getElementById('main')
+    const mainSection = document.getElementById("main")
 
     if (mainSection && scrollPosition > mainSection.offsetTop + mainSection.offsetHeight) {
       setPastMain(true)
@@ -40,58 +40,58 @@ const Header = () => {
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', toggleScroll)
+    window.addEventListener("scroll", toggleScroll)
 
     return () => {
-      window.removeEventListener('scroll', toggleScroll)
+      window.removeEventListener("scroll", toggleScroll)
     }
   }, [])
 
   const scrollToggle =
-    scroll || pastMain ? `${styles.header} ${styles.header_scroll} ${pastMain ? styles.header_white : ''}` : styles.header
+    scroll || pastMain ? `${styles.header} ${styles.header_scroll} ${pastMain ? styles.header_white : ""}` : styles.header
 
   return (
     <header className={scrollToggle}>
-      <div className='container'>
+      <div className="container">
         <div className={styles.header_wrapper}>
           <BurgerMenu isOpen={isMenuOpen} onClose={toggleMenu} id={styles.mobile_only} linkClass={styles.header_link} />
-          <Link href='/#main'>
+          <Link href="/#main">
             <Logo />
           </Link>
-          <Link href='#about' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/#about" className={styles.header_link} id={styles.desktop_only}>
             О студии
           </Link>
-          <Link href='/#portfolio' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/#portfolio" className={styles.header_link} id={styles.desktop_only}>
             Наши работы
           </Link>
-          <Link href='/#services' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/#services" className={styles.header_link} id={styles.desktop_only}>
             Услуги
           </Link>
-          <Link href='/#reviews' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/#reviews" className={styles.header_link} id={styles.desktop_only}>
             Отзывы
           </Link>
-          <Link href='/blog' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/blog" className={styles.header_link} id={styles.desktop_only}>
             Блог о съемке
           </Link>
-          <Link href='#contacts' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="#contacts" className={styles.header_link} id={styles.desktop_only}>
             Контакты
           </Link>
-          <Link href='/calendar' className={styles.header_link} id={styles.desktop_only}>
+          <Link href="/calendar" className={styles.header_link} id={styles.desktop_only}>
             <HeaderButton>Календарь съемок</HeaderButton>
           </Link>
           <div className={styles.header_contacts}>
-            <Link href='/' className={styles.header_link} id={styles.manager_number}>
+            <Link href="/" className={styles.header_link} id={styles.manager_number}>
               {managerNumber}
             </Link>
             <div className={styles.header_contacts_icons}>
-              <Link href='/'>
-                <Image src={whatsappLogo} alt='WhatsApp' />
+              <Link href="/">
+                <Image src={whatsappLogo} alt="WhatsApp" />
               </Link>
-              <Link href='/'>
-                <Image src={vkLogo} alt='VKontakte' />
+              <Link href="/">
+                <Image src={vkLogo} alt="VKontakte" />
               </Link>
-              <Link href='/'>
-                <Image src={telegramLogo} alt='Telegram' />
+              <Link href="/">
+                <Image src={telegramLogo} alt="Telegram" />
               </Link>
             </div>
           </div>
